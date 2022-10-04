83abadde1303776707d2ab514ff07816783c3623
Matsura Yuma <lemonburst1958@gmail.com>
2022-10-04 11:50:10 +0900

    Bootstrap

---
+++ src/components/App.tsx
@@ -0,0 +1,7 @@
+import styles from "./App.module.scss";
+
+function App() {
+  return <div className={styles.container}>hello world</div>;
+}
+
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+import "./styles/reset.css";
+import "./styles/tokens.css";
+import "./styles/globals.css";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
