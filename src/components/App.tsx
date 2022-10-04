c8e6f1577bc243917643dfc561661e01880f6b09
Matsura Yuma <lemonburst1958@gmail.com>
2022-10-04 16:49:22 +0900

    Reduce description text

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -40,7 +40,7 @@ function App() {
             },
           ]}
           name="White T-Shirt"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud."
           price="$49"
           sizes={["xs", "s", "m", "l", "xl"]}
           rating={3}
