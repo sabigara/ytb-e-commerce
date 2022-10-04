41de4a3ba1f46cd76f01e0a7560cb7230530224a
Matsura Yuma <lemonburst1958@gmail.com>
2022-10-04 14:35:31 +0900

    Overall layout

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -6,30 +6,49 @@ import ProductDetail from "./ProductDetail";
 function App() {
   return (
     <div className={styles.container}>
-      <Carousel
-        images={["/t-shirt-0.jpg", "/t-shirt-1.jpg", "/t-shirt-2.jpg"]}
-      />
-      <ProductDetail
-        categories={[
-          {
-            href: "/home",
-            label: "Home",
-          },
-          {
-            href: "/men",
-            label: "Men",
-          },
-          {
-            href: "/men/t-shirts",
-            label: "T-Shirts",
-          },
-        ]}
-        colors={[]}
-        name="White T-Shirt"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-        price="$49"
-        sizes={["xs", "s", "m", "l", "xl"]}
-      />
+      <div className={styles.inner}>
+        <Carousel
+          images={["/t-shirt-0.jpg", "/t-shirt-1.jpg", "/t-shirt-2.jpg"]}
+        />
+        <ProductDetail
+          categories={[
+            {
+              href: "/home",
+              label: "Home",
+            },
+            {
+              href: "/men",
+              label: "Men",
+            },
+            {
+              href: "/men/t-shirts",
+              label: "T-Shirts",
+            },
+          ]}
+          colors={[
+            {
+              label: "White",
+              hex: "#000000",
+            },
+            {
+              label: "Gray",
+              hex: "#eeeeee",
+            },
+            {
+              label: "Black",
+              hex: "#000000",
+            },
+            {
+              label: "Red",
+              hex: "#ff0000",
+            },
+          ]}
+          name="White T-Shirt"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+          price="$49"
+          sizes={["xs", "s", "m", "l", "xl"]}
+        />
+      </div>
     </div>
   );
 }
